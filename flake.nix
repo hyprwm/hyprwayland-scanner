516cf4ef20d3eb4d1ae811a6c0069b71a602871f
Mihai Fufezan <mihai@fufexan.net>
2024-04-20 13:40:18 +0300

    Nix: init

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "A Hyprland version of wayland-scanner in and for C++";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default-linux";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+  }: let
+    inherit (nixpkgs) lib;
+    eachSystem = lib.genAttrs (import systems);
+    pkgsFor = eachSystem (system:
+      import nixpkgs {
+        localSystem.system = system;
+        overlays = with self.overlays; [hyprwayland-scanner];
+      });
+    mkDate = longDate: (lib.concatStringsSep "-" [
+      (builtins.substring 0 4 longDate)
+      (builtins.substring 4 2 longDate)
+      (builtins.substring 6 2 longDate)
+    ]);
+  in {
+    overlays = {
+      default = self.overlays.hpyrwayland-scanner;
+      hyprwayland-scanner = final: prev: {
+        hyprwayland-scanner = final.callPackage ./nix/default.nix {
+          stdenv = final.gcc13Stdenv;
+          version = "0.pre" + "+date=" + (mkDate (self.lastModifiedDate or "19700101")) + "_" + (self.shortRev or "dirty");
+        };
+      };
+    };
+
+    packages = eachSystem (system: {
+      default = self.packages.${system}.hyprwayland-scanner;
+      inherit (pkgsFor.${system}) hyprwayland-scanner;
+    });
+
+    formatter = eachSystem (system: pkgsFor.${system}.alejandra);
+  };
+}
---
+++ nix/default.nix
@@ -0,0 +1,27 @@
+{
+  lib,
+  stdenv,
+  cmake,
+  pkg-config,
+  pugixml,
+  version ? "git",
+  doCheck ? false,
+}:
+stdenv.mkDerivation {
+  pname = "hyprwayland-scanner";
+  inherit version doCheck;
+  src = ../.;
+
+  nativeBuildInputs = [
+    cmake
+    pkg-config
+    pugixml
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/hyprwm/hyprwayland-scanner";
+    description = "A Hyprland version of wayland-scanner in and for C++";
+    license = licenses.bsd3;
+    platforms = platforms.linux;
+  };
+}
