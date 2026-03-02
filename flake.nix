0a692d4a645165eebd65f109146b8861e3a925e7
Mihai Fufezan <mihai@fufexan.net>
2026-03-02 15:57:15 +0200

    treewide: alejandra -> nixfmt

--- flake.nix
+++ flake.nix
@@ -6,33 +6,40 @@
     systems.url = "github:nix-systems/default-linux";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    systems,
-  }: let
-    inherit (nixpkgs) lib;
-    eachSystem = lib.genAttrs (import systems);
-    pkgsFor = eachSystem (system:
-      import nixpkgs {
-        localSystem.system = system;
-        overlays = with self.overlays; [hyprwayland-scanner];
-      });
-    pkgsCrossFor = eachSystem (system: crossSystem:
-      import nixpkgs {
-        localSystem = system;
-        crossSystem = crossSystem;
-        overlays = with self.overlays; [hyprwayland-scanner];
-      });
-  in {
-    overlays = import ./nix/overlays.nix {inherit lib self;};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+    }:
+    let
+      inherit (nixpkgs) lib;
+      eachSystem = lib.genAttrs (import systems);
+      pkgsFor = eachSystem (
+        system:
+        import nixpkgs {
+          localSystem.system = system;
+          overlays = with self.overlays; [ hyprwayland-scanner ];
+        }
+      );
+      pkgsCrossFor = eachSystem (
+        system: crossSystem:
+        import nixpkgs {
+          localSystem = system;
+          crossSystem = crossSystem;
+          overlays = with self.overlays; [ hyprwayland-scanner ];
+        }
+      );
+    in
+    {
+      overlays = import ./nix/overlays.nix { inherit lib self; };
 
-    packages = eachSystem (system: {
-      default = self.packages.${system}.hyprwayland-scanner;
-      inherit (pkgsFor.${system}) hyprwayland-scanner;
-      hyprwayland-scanner-cross = (pkgsCrossFor.${system} "aarch64-linux").hyprwayland-scanner;
-    });
+      packages = eachSystem (system: {
+        default = self.packages.${system}.hyprwayland-scanner;
+        inherit (pkgsFor.${system}) hyprwayland-scanner;
+        hyprwayland-scanner-cross = (pkgsCrossFor.${system} "aarch64-linux").hyprwayland-scanner;
+      });
 
-    formatter = eachSystem (system: pkgsFor.${system}.alejandra);
-  };
+      formatter = eachSystem (system: pkgsFor.${system}.nixfmt-tree);
+    };
 }
